e29f1a5808c466ee9d154a175d1da3eca19017dd
sanskruti-chikhlonde <sanskruti0804@gmail.com>
2026-08-22 23:38:42 +0530

    add X and O player turns

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const cells = document.querySelectorAll(".cell");
 
+let currentPlayer = "X";
+
 cells.forEach(function(cell) {
     cell.addEventListener("click", function() {
-        console.log("Cell clicked");
+
+        cell.textContent = currentPlayer;
+
+        if (currentPlayer === "X") {
+            currentPlayer = "O";
+        } else {
+            currentPlayer = "X";
+        }
+
     });
 });
