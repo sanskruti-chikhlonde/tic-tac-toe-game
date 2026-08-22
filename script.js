18b02eee3214b39b8cbf20686498226e13463bab
sanskruti-chikhlonde <sanskruti0804@gmail.com>
2026-08-22 23:48:55 +0530

    add tic tac toe winning combinations

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const turnText = document.querySelector(".turn");
 
 let currentPlayer = "X";
 
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 cells.forEach(function(cell) {
     cell.addEventListener("click", function() {
 
